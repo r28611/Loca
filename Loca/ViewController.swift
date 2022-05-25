c8441a365aa3a0e6f7d7ca02eeb8c7d9ad8c1824
Margarita Novokh <r28611@gmail.com>
2022-05-25 23:59:40 +0200

    find Zhdun

--- Loca/ViewController.swift
+++ Loca/ViewController.swift
@@ -10,13 +10,34 @@ import GoogleMaps
 
 class ViewController: UIViewController {
     
+    // 'Zhdun' (Homunculus Loxodontus) 52.1662, 4.4784
+    private let zhdunCoordinate = CLLocationCoordinate2D(latitude: 52.1662, longitude: 4.4784)
+    private var marker: GMSMarker?
+    
     @IBOutlet private var mapView: GMSMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction private func goTo(_ sender: UIButton) {
+        findZhdun()
+        addMarker()
+    }
+    
+    private func findZhdun() {
+        let camera = GMSCameraPosition.camera(withTarget: zhdunCoordinate, zoom: 10)
+        mapView.camera = camera
+    }
+    
+    private func addMarker() {
+        marker = GMSMarker(position: zhdunCoordinate)
+        
+        marker?.icon = UIImage(named: "zhdun-icon")
+        marker?.title = "Homunculus Loxodontus"
+        marker?.snippet = "Waiting for you..."
+        
+        marker?.map = mapView
+    }
 }
 
