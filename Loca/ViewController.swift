b818e5c1ed3c47c3c6607fdfe2e93493464c1a75
Margarita Novokh <r28611@gmail.com>
2022-05-29 22:55:22 +0200

    animated camera transition on coordinates

--- Loca/ViewController.swift
+++ Loca/ViewController.swift
@@ -15,20 +15,26 @@ class ViewController: UIViewController {
     private var marker: GMSMarker?
     
     @IBOutlet private var mapView: GMSMapView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
     }
     
     @IBAction private func goTo(_ sender: UIButton) {
-        findZhdun()
-        addZhdun()
+        
+        if marker == nil {
+            findZhdun()
+            addZhdun()
+        } else {
+            removeMarker()
+        }
     }
     
     private func findZhdun() {
-        let camera = GMSCameraPosition.camera(withTarget: zhdunCoordinate, zoom: 10)
-        mapView.camera = camera
+        mapView.animate(to: GMSCameraPosition(latitude: zhdunCoordinate.latitude,
+                                              longitude: zhdunCoordinate.longitude,
+                                              zoom: 10))
     }
     
     private func addZhdun() {
@@ -37,8 +43,14 @@ class ViewController: UIViewController {
         marker?.icon = UIImage(named: "zhdun-icon")
         marker?.title = "Homunculus Loxodontus"
         marker?.snippet = "Waiting for you..."
+        marker?.groundAnchor = CGPoint(x: 0.5, y: 0.5)
         
         marker?.map = mapView
+    }
+    
+    func removeMarker() {
+        marker?.map = nil
+        marker = nil
     }
 }
 
