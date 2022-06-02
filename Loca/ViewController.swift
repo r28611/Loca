1394d6a6b40f715710fd890544a34d9d4e7e1ec3
Margarita Novokh <r28611@gmail.com>
2022-06-03 00:35:01 +0200

    added routes (with mock json)

--- Loca/ViewController.swift
+++ Loca/ViewController.swift
@@ -33,16 +33,15 @@ class ViewController: UIViewController {
         mapView.isMyLocationEnabled = true
         
         mapView.delegate = self
-        
         do {
-          // Set the map style by passing the URL of the local file.
-          if let styleURL = Bundle.main.url(forResource: "style", withExtension: "json") {
-            mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
-          } else {
-            NSLog("Unable to find style.json")
-          }
+            // Set the map style by passing the URL of the local file.
+            if let styleURL = Bundle.main.url(forResource: "style", withExtension: "json") {
+                mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+            } else {
+                NSLog("Unable to find style.json")
+            }
         } catch {
-          NSLog("One or more of the map styles failed to load. \(error)")
+            NSLog("One or more of the map styles failed to load. \(error)")
         }
     }
     
@@ -54,6 +53,10 @@ class ViewController: UIViewController {
         } else {
             removeMarker()
         }
+    }
+    
+    @IBAction private func routeDidTapped() {
+        drawPolyline()
     }
     
     @IBAction private func updateLocation(_ sender: UIBarButtonItem) {
@@ -96,6 +99,41 @@ class ViewController: UIViewController {
         locationManager?.requestWhenInUseAuthorization()
         locationManager?.allowsBackgroundLocationUpdates = true
     }
+    
+    func drawPolyline() {
+        
+        let coordinates = geoJson.map({CLLocationCoordinate2D(latitude: $0.last!, longitude: $0.first!)})
+        
+        let chunkSize = 3
+        let chunkedCoordinates = coordinates.chunked(into: chunkSize)
+        
+        let path = GMSMutablePath()
+        
+        for chunk in chunkedCoordinates {
+            for coordinate in chunk {
+                path.add(coordinate)
+            }
+            
+            if chunk.count == 3 {
+                let location1 = CLLocation(latitude: chunk[1].latitude, longitude: chunk[1].longitude)
+                let location2 = CLLocation(latitude: chunk[2].latitude, longitude: chunk[2].longitude)
+                
+                if !GMSGeometryIsLocationOnPath(chunk[1], path, false) ||
+                    !GMSGeometryIsLocationOnPath(chunk[2], path, false) ||
+                    location2.distance(from: location1) < 1.5 {
+                    continue
+                }
+            }
+        }
+        
+        let polyline = GMSPolyline(path: path)
+        polyline.strokeColor = UIColor.orange
+        polyline.strokeWidth = 5.0
+        polyline.map = self.mapView
+        
+        let bounds = GMSCoordinateBounds(path: path)
+        self.mapView.animate(with: GMSCameraUpdate.fit(bounds, withPadding: 50.0))
+    }
 }
 
 extension ViewController: GMSMapViewDelegate {
@@ -136,5 +174,14 @@ extension ViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error)
+    }
+}
+
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
     }
 }
