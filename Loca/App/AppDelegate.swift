706b6602d61997921aa414831ef6f045a2c34516
Margarita Novokh <r28611@gmail.com>
2022-07-25 23:29:35 +0200

    user notifications

--- Loca/App/AppDelegate.swift
+++ Loca/App/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("Input here your API key")
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            guard granted else {
+                print("Alerts didn't allowed")
+                return
+            }
+            self.sendNotificatioRequest(content: self.makeNotificationContent(),
+                                        trigger: self.makeIntervalNotificatioTrigger())
+        }
         return true
     }
 
@@ -33,6 +43,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func makeNotificationContent() -> UNNotificationContent {
+        let content = UNMutableNotificationContent()
+        content.title = "Your route is recordind"
+        content.body = "Check or stop your route"
+        content.badge = 1
+        return content
+    }
+    
+    func makeIntervalNotificatioTrigger() -> UNNotificationTrigger {
+        return UNTimeIntervalNotificationTrigger(timeInterval: 60,
+                                                 repeats: true)
+    }
+    
+    func sendNotificatioRequest( content: UNNotificationContent, trigger: UNNotificationTrigger) {
+        
+        let request = UNNotificationRequest( identifier: "recording",
+                                             content: content,
+                                             trigger: trigger)
+        let center = UNUserNotificationCenter.current()
+        
+        center.add(request) { error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
 }
 
